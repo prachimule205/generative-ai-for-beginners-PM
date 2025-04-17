79f2f50d43be292e5cf6c300bcddeb7138744487
prachimule205 <prachi.pachad@gmail.com>
2025-04-17 15:17:31 +0530

    print

--- 06-text-generation-apps/test.py
+++ 06-text-generation-apps/test.py
@@ -8,3 +8,4 @@ openai.api_type = 'azure'
 openai.api_key = os.environ["OPENAI_API_KEY"]
 openai.api_version = '2023-05-15'
 openai.api_base = os.getenv("API_BASE")
+print("API Base:", openai.api_base)
